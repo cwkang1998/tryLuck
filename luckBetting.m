2de95bc0bb5b25e6810295840e39e66ff42fe5c9
cwkang1998 <cwk1998@hotmail.com>
2017-03-28 15:32:50 +0800

    Main Game Completed, Working on scoring and highscore

--- luckBetting.m
+++ luckBetting.m
@@ -25,9 +25,11 @@ function main()
 		choice = menu('<Shooting Star>', 'Start Game', 'Instructions', 'Highscore');
         switch(choice)
 		case 1
+			%Ends when no life is left.
 			while(life>0)
 				[life,score] = game(life,score);
-			end			
+			end
+			
 		case 2
 			instruction();
 		case 3
@@ -68,7 +70,6 @@ function[life, score] = game(life, score)
 	%initialise difficulity
 	d = difficulity();
 	figure('Name', 'Try Your Luck!', 'MenuBar', 'none');
-	
 	%Animation
     for i=0:27
         clf;
@@ -86,11 +87,20 @@ function[life, score] = game(life, score)
         pause(1/60);
     end
 	
+	%Game Start Prompt
+	title(gca,'Game Start! xD');
+	pause(2);
+	
     %Show two options
 	text(6, 20,'Reveal A Card');
-	text(19.25, 20,'Forfeit Match');
 	rectangle('Position', [5 17.25 7 5]);
+	text(19.25, 20,'Forfeit Match');
 	rectangle('Position', [18 17.25 7 5]);
+	text(28, 29,'Exit');
+	rectangle('Position', [27.75 28.5 2 1]);
+	
+	%Show player's life
+	text(2,29,sprintf('Life : %d',life));
 	
 	%Generate random computer's number
 	c_number=randi(20+(d*10)); 
@@ -102,26 +112,58 @@ function[life, score] = game(life, score)
 	
 	%Initialise coordinates for the numbers to appear
 	coor = [8.5 12.5 16.5 20.5];
-  
-	%Prompts the user to click on a card.
-	title(gca,'Click reveal to reveal a card''s number.');
 
 	%Accept mouse click as input to determine the user's action
 	a = 1;
 	shown = 0;
-	while shown<4
+	while shown<2+d		  
+		%Prompts the user to click on a card.
+		title(gca,'Click reveal to reveal a card''s number.');
 		[x,y]=ginput(1);
 		if(x>5&&x<12&&y>17.25&&y<22.25)
+			title(gca,'Your card number is...');
 			pause(1);
 			text(coor(a), 6.5,sprintf('%d',u_cardNumber(a)));
+			title(gca,sprintf('%d!!!',u_cardNumber(a)));
+			pause(2);
 			a = a + 1;
 			shown = shown + 1;
 		end
 		if(x>18&&x<25&&y>17.25&&y<22.25)
-			isForfeit = 1;
+			title(gca,'Player forfieted the match :(');
+			%Show all card on hand
+			while(a<=2+d)
+				text(coor(a), 6.5,sprintf('%d',u_cardNumber(a)));
+				a = a+1;
+			end
 			break;
 		end
+		if(x>27.75&&x<29.75&&y>28.5&&y<29.5)
+			title(gca,'Click Exit once more to confirm exit!');
+			[x, y] = ginput(1);
+			if(x>27.75&&x<29.75&&y>28.5&&y<29.5)
+				life = 0;
+				close all;
+				return;
+			end
+		end
 	end
+	%Reveal computer's number
+	pause(1);
+	title(gca, sprintf('The computer''s number is : %d !', c_number));
+	pause(2);
+	%Check scoring condition
+	if(shown==2+d)
+		if(sum(u_cardNumber)>=c_number)
+			title(gca, sprintf('Congratz!!! Your total sum of numbers (%d) is larger than computer''s number (%d) !',sum(u_cardNumber),c_number));
+			score = score + 1;
+		else
+			title(gca, sprintf('Too bad, Your total sum of numbers (%d) is smaller than computer''s number (%d) !',sum(u_cardNumber),c_number));
+			life = life - 1;
+		end
+	end
+	pause(3);
+	close all;
 end
 
 %Show game over dialog box
